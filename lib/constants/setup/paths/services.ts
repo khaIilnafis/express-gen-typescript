b4c0900b7fc7f25f02c213983de6409960c4e3cc
Khalil Brown <khalilnafis@gmail.com>
2025-03-02 01:48:16 -0800

    mv constants into modules

--- lib/constants/setup/paths/services.ts
+++
@@ -1,9 +0,0 @@
-export interface ServicesFilePaths {
-	INDEX: string;
-	INDEX_TEMPLATE: string;
-};
-
-export const SERVICES = Object.freeze({
-	INDEX: "index.ts",
-	INDEX_TEMPLATE: "index.ast.ts"
-  }as const) satisfies ServicesFilePaths;
